86d838ef3a56d885b405d46d264994a4f2c8a502
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-24 10:26:57 -0300

    Inicio do curso de JS e operacoes

---
+++ Javascript/2_OPERADORES/js/scripts.js
@@ -0,0 +1,10 @@
+// Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -128);
+
+// Operações aritméticas
+console.log(2+4)
+console.log(10-5)
+console.log(5/1092)
+console.log(4.5*9.222)

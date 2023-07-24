046c4c74cd09efd6f529959a32d57085c8655b64
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-24 20:44:12 -0300

    Um pouco mais de closure

--- Javascript/4_FUNCOES/js/scripts.js
+++ Javascript/4_FUNCOES/js/scripts.js
@@ -151,3 +151,19 @@ function someFunction() {
 }
 
 someFunction();
+
+const multiplicationClosure = (n) => {
+    return (m) => {
+        return n * m;
+    }
+}
+
+const c1 = multiplicationClosure(5);
+const c2 = multiplicationClosure(10);
+
+console.log(c1);
+console.log(c2);
+
+console.log(c1(5));
+console.log(c2(10));
+

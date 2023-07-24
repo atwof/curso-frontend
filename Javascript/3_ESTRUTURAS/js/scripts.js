8ca58823378dc67d8390746ee74775ebb579dfc2
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-24 13:06:18 -0300

    Regras para declaracao de variaveis

--- Javascript/3_ESTRUTURAS/js/scripts.js
+++ Javascript/3_ESTRUTURAS/js/scripts.js
@@ -6,5 +6,23 @@ nome = "Alisson";
 console.log(nome);
 
 const idade = 29;
-
 console.log(idade);
+
+// Não é permitido dessa forma
+//let 2teste = "Alisson";
+//let @teste = "Alisson";
+
+// Não é o mais indicado
+let a = 10, b = 20, c = 30;
+console.log(a,b,c);
+
+// Case sensitive
+const nomecompleto = "Alisson F. Fritzen";
+const nomeCompleto = "Alisson"
+console.log(nomecompleto, nomeCompleto);
+
+// Permitido
+let _teste = "teste";
+let $teste = "teste";
+
+console.log(_teste, $teste);

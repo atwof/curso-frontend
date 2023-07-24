634891de6e15d3f87c3289afca58224834b72530
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-24 10:57:59 -0300

    Interpolacao

--- Javascript/2_OPERADORES/js/scripts.js
+++ Javascript/2_OPERADORES/js/scripts.js
@@ -4,29 +4,33 @@ console.log(typeof 5.14);
 console.log(typeof -128);
 
 // Operações aritméticas
-console.log(2+4)
-console.log(10-5)
-console.log(5/1092)
-console.log(4.5*9.222)
+console.log(2+4);
+console.log(10-5);
+console.log(5/1092);
+console.log(4.5*9.222);
 
 // Special numbers
-console.log(typeof Infinity)
-console.log(typeof -Infinity)
-console.log(typeof NaN)
-console.log(2/0)
-console.log(10 * "s")
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
+console.log(2/0);
+console.log(10 * "s");
 
 // Strings
-console.log("Alisson")
-console.log('Alisson')
-console.log(`alisson`)
-console.log(typeof "Alisson")
-console.log(typeof 'Alisson')
+console.log("Alisson");
+console.log('Alisson');
+console.log(`alisson`);
+console.log(typeof "Alisson");
+console.log(typeof 'Alisson');
 
 // Mais strings
-console.log("Testando o contra barra n \n nesse momento")
-console.log("Espaçamento de \t tab")
+console.log("Testando o contra barra n \n nesse momento");
+console.log("Espaçamento de \t tab");
 
 // Concatenação
-console.log("Oi, " + "tudo " + "bem?")
-console.log(`Testando ` + `com a ` + `crase`)
+console.log("Oi, " + "tudo " + "bem?");
+console.log(`Testando ` + `com a ` + `crase`);
+
+// Interpolação de strings ou template strings
+console.log(`A soma de 2 + 2 é: ${2+2}`);
+console.log(`Podemos executar qualquer coisa aqui ${console.log(2222)}`);

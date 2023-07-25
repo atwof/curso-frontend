36e8a3d474ec71e2c1e2920c4d6cbf3d9080f44e
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-25 16:19:48 -0300

    debug - console.log

--- Javascript/7_DEBUG/js/scripts.js
+++ Javascript/7_DEBUG/js/scripts.js
@@ -5,3 +5,24 @@
 const opa = "Teste";
 
 // const undefined = 10;
+
+// console.log
+let a = 1;
+let b = 2;
+
+if(a === 1) {
+    a = b + 2;
+}
+
+console.log(a);
+
+for (let i = 0; i < b; i++) {
+    a = a + 2;
+    console.log(a);
+}
+
+if(a > 5) {
+    a = 25;
+}
+
+console.log(a);

d3924b0f98e7c66be9bb93272d761aa3092668eb
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-25 15:39:00 -0300

    Classes ES6

--- Javascript/6_OOP/js/scripts.js
+++ Javascript/6_OOP/js/scripts.js
@@ -89,3 +89,14 @@ Cachorro.prototype.uivar = function() {
 }
 
 husk.uivar();
+
+// Classes ES6
+class CachorroClasse {
+    constructor(nome, raca) {
+        this.nome = nome,
+        this.raca = raca
+    }
+}
+
+const jeff = new CachorroClasse("Jeff", "Labrador");
+console.log(jeff);

c200016c6dd67881bca7591b34f886151aaabc12
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-25 09:41:47 -0300

    Incio dos arrays e objetos

---
+++ Javascript/5_ARRAY_OBJETOS/js/scripts.js
@@ -0,0 +1,18 @@
+// Array
+const lista = [1,2,3,4,5];
+console.log(lista);
+console.log(typeof lista);
+
+const items = ["Matheus", 12, true, "Teste"];
+console.log(items);
+
+// Acessar item em um array
+const arr = ['a','b','c','d','e'];
+console.log(arr[0]);
+
+// Propriedades
+const number = [1,2,3];
+console.log(number.length);
+
+const myString = "Alisson"
+console.log(myString.length);

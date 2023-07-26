95544c94b64361b06f0dd22a2a10005ca6c67ea3
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-26 10:13:45 -0300

    Selecionando elementos por tag

--- Javascript/9_DOM/js/scripts.js
+++ Javascript/9_DOM/js/scripts.js
@@ -1,5 +1,9 @@
-// Movendo-se pelo DOM
+// Movendo-se pelo DOM -> Para HTMLs simples
 console.log(document.body);
 console.log(document.body.childNodes);
 console.log(document.body.childNodes[1].childNodes);
 console.log(document.body.childNodes[1].childNodes[1].textContent)
+
+// Selecionando elementos por TAG
+const elementosPorTag = document.getElementsByTagName("li");
+console.log(elementosPorTag);

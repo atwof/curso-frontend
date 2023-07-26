e8cd24c228ecbb056147be4b5a4ad53229700b34
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-26 11:06:31 -0300

    Removendo eventos

--- Javascript/10_EVENTOS/js/scripts.js
+++ Javascript/10_EVENTOS/js/scripts.js
@@ -3,3 +3,16 @@ const btn = document.querySelector("#my-button");
 btn.addEventListener("click", function() {
     alert("Clicou aqui");
 });
+
+// Removendo evento
+const secondBtn = document.querySelector("#btn");
+function imprimirMensagem() {
+    alert("Teste");
+}
+secondBtn.addEventListener("click", imprimirMensagem);
+
+const thirdBtn = document.querySelector("#other-btn");
+thirdBtn.addEventListener("click", () => {
+    alert("Evento removido");
+    secondBtn.removeEventListener("click", imprimirMensagem);
+});

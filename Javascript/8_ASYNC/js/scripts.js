ad504428e1e15599f6138d9aec5b38840d59329b
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-25 22:28:19 -0300

    setInterval - Executa em determinado tempo infinitamente

--- Javascript/8_ASYNC/js/scripts.js
+++ Javascript/8_ASYNC/js/scripts.js
@@ -1,6 +1,13 @@
-// setTimeOut
+// setTimeOut -> Executado após um tempo
 console.log("Ainda nao executou");
 setTimeout(function () {
-    console.log("Requisicao assincrona");
+    console.log("Executou o setTimeOut");
 }, 2000)
 console.log("Ainda nao executou 2");
+
+// setInterval -> Executa algo de tempo em tempo infinitamente
+console.log("Ainda nao comecou");
+// setInterval(function() {
+//     console.log("Executou o setInterval");
+// }, 3000)
+console.log("Ainda nao comecou");

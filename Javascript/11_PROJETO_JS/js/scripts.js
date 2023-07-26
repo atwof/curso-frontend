7ca39b1f75f66cec72d9e9ef815770876d98189b
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-26 14:23:45 -0300

    Finalizacao do projeto da tabuada

--- Javascript/11_PROJETO_JS/js/scripts.js
+++ Javascript/11_PROJETO_JS/js/scripts.js
@@ -2,8 +2,31 @@
 const multiForm = document.querySelector("#multiplication-form");
 const numberInput = document.querySelector("#number");
 const multiInput = document.querySelector("#multiplicator");
+const multiplicationTable = document.querySelector("#multiplication-operations");
+const multiplicationTitle = document.querySelector("#multiplication-title span");
 
 // Funções
+const createTable = (number, multiplicatorNumber) => {
+    multiplicationTable.innerHTML = "";
+
+    for(i = 1; i <= multiplicatorNumber; i++) {
+        const result = number * i;
+
+        const template = 
+        `<div class="row">
+            <div class="operation">${number} x ${i} = </div>
+            <div class="result">${result}</div>
+        </div>`;
+
+        const parser = new DOMParser();
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+        multiplicationTable.appendChild(row);
+    }
+
+    multiplicationTitle.innerText = number;
+}
 
 // Eventos
 multiForm.addEventListener("submit", (e) => {
@@ -11,5 +34,8 @@ multiForm.addEventListener("submit", (e) => {
     const multiplicationNumber = +numberInput.value;
     const multiplicatorNumber = multiInput.value;
 
-    if(!multiplicationNumber || !multiplicatorNumber) return;
+    if(!multiplicationNumber || !multiplicatorNumber ||
+       multiplicationNumber < 1 || multiplicatorNumber < 1) return;
+
+    createTable(multiplicationNumber, multiplicatorNumber);
 });

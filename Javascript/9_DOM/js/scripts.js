2f994c373f3458fa8700d0e03cb4fb3a0b0efbcf
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-26 10:08:29 -0300

    Iniciando no DOM

---
+++ Javascript/9_DOM/js/scripts.js
@@ -0,0 +1,5 @@
+// Movendo-se pelo DOM
+console.log(document.body);
+console.log(document.body.childNodes);
+console.log(document.body.childNodes[1].childNodes);
+console.log(document.body.childNodes[1].childNodes[1].textContent)

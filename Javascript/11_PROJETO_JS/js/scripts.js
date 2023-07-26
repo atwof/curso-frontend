41f94368e4ce060c19e4f4bd9a21d04db0cd55b7
Alisson F. Fritzen <alisson.fritzen@Alissons-MacBook-Pro.local>
2023-07-26 14:09:11 -0300

    Inicio do js

--- Javascript/11_PROJETO_JS/js/scripts.js
+++ Javascript/11_PROJETO_JS/js/scripts.js
@@ -1,1 +1,15 @@
-console.log("teste");
+// Selecionar os elementos
+const multiForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiInput = document.querySelector("#multiplicator");
+
+// Funções
+
+// Eventos
+multiForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const multiplicationNumber = +numberInput.value;
+    const multiplicatorNumber = multiInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+});
